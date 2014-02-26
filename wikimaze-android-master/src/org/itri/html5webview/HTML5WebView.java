f5be121daf466e19a2ae3dc23b23c498e54257b6
Roy Sun <onecoders@gmail.com>
2014-02-26 09:34:23 +0800

    add progress dialog when webview's loading

--- wikimaze-android-master/src/org/itri/html5webview/HTML5WebView.java
+++ wikimaze-android-master/src/org/itri/html5webview/HTML5WebView.java
@@ -1,6 +1,7 @@
 package org.itri.html5webview;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -19,54 +20,62 @@ import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 
 public class HTML5WebView extends WebView {
-	
-	private Context 							mContext;
-	private MyWebChromeClient					mWebChromeClient;
-	private View								mCustomView;
-	private FrameLayout							mCustomViewContainer;
-	private WebChromeClient.CustomViewCallback 	mCustomViewCallback;
-	
-	private FrameLayout							mContentView;
-	private FrameLayout							mBrowserFrameLayout;
-	private FrameLayout							mLayout;
-	
-    static final String LOGTAG = "HTML5WebView";
-	    
+
+	private Context mContext;
+	private MyWebChromeClient mWebChromeClient;
+	private View mCustomView;
+	private FrameLayout mCustomViewContainer;
+	private WebChromeClient.CustomViewCallback mCustomViewCallback;
+
+	private FrameLayout mContentView;
+	private FrameLayout mBrowserFrameLayout;
+	private FrameLayout mLayout;
+
+	static final String LOGTAG = "HTML5WebView";
+
+	private ProgressDialog mProgressDialog;
+
 	private void init(Context context) {
-		mContext = context;		
+		mContext = context;
 		Activity a = (Activity) mContext;
-		
+
 		mLayout = new FrameLayout(context);
-		
-		mBrowserFrameLayout = (FrameLayout) LayoutInflater.from(a).inflate(R.layout.custom_screen, null);
-		mContentView = (FrameLayout) mBrowserFrameLayout.findViewById(R.id.main_content);
-		mCustomViewContainer = (FrameLayout) mBrowserFrameLayout.findViewById(R.id.fullscreen_custom_content);
-		
+
+		mProgressDialog = new ProgressDialog(context);
+		mProgressDialog.setMessage("Loading...");
+
+		mBrowserFrameLayout = (FrameLayout) LayoutInflater.from(a).inflate(
+				R.layout.custom_screen, null);
+		mContentView = (FrameLayout) mBrowserFrameLayout
+				.findViewById(R.id.main_content);
+		mCustomViewContainer = (FrameLayout) mBrowserFrameLayout
+				.findViewById(R.id.fullscreen_custom_content);
+
 		mLayout.addView(mBrowserFrameLayout, COVER_SCREEN_PARAMS);
 
 		mWebChromeClient = new MyWebChromeClient();
-	    setWebChromeClient(mWebChromeClient);
-	    
-	    setWebViewClient(new MyWebViewClient());
-	       
-	    // Configure the webview
-	    WebSettings s = getSettings();
-	    s.setBuiltInZoomControls(true);
-	    s.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-	    s.setUseWideViewPort(true);
-	    s.setLoadWithOverviewMode(true);
-	    s.setSavePassword(true);
-	    s.setSaveFormData(true);
-	    s.setJavaScriptEnabled(true);
-	    
-	    // enable navigator.geolocation 
-	    s.setGeolocationEnabled(true);
-	    s.setGeolocationDatabasePath("/data/data/org.itri.html5webview/databases/");
-	    
-	    // enable Web Storage: localStorage, sessionStorage
-	    s.setDomStorageEnabled(true);
-	    
-	    mContentView.addView(this);
+		setWebChromeClient(mWebChromeClient);
+
+		setWebViewClient(new MyWebViewClient());
+
+		// Configure the webview
+		WebSettings s = getSettings();
+		s.setBuiltInZoomControls(true);
+		s.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+		s.setUseWideViewPort(true);
+		s.setLoadWithOverviewMode(true);
+		s.setSavePassword(true);
+		s.setSaveFormData(true);
+		s.setJavaScriptEnabled(true);
+
+		// enable navigator.geolocation
+		s.setGeolocationEnabled(true);
+		s.setGeolocationDatabasePath("/data/data/org.itri.html5webview/databases/");
+
+		// enable Web Storage: localStorage, sessionStorage
+		s.setDomStorageEnabled(true);
+
+		mContentView.addView(this);
 	}
 
 	public HTML5WebView(Context context) {
@@ -83,118 +92,134 @@ public class HTML5WebView extends WebView {
 		super(context, attrs, defStyle);
 		init(context);
 	}
-	
+
 	public FrameLayout getLayout() {
 		return mLayout;
 	}
-	
-    public boolean inCustomView() {
+
+	public boolean inCustomView() {
 		return (mCustomView != null);
 	}
-    
-    public void hideCustomView() {
+
+	public void hideCustomView() {
 		mWebChromeClient.onHideCustomView();
 	}
-    
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-    	if (keyCode == KeyEvent.KEYCODE_BACK) {
-    		if ((mCustomView == null) && canGoBack()){
-    			goBack();
-    			return true;
-    		}
-    	}
-    	return super.onKeyDown(keyCode, event);
-    }
 
-    private class MyWebChromeClient extends WebChromeClient {
-		private Bitmap 		mDefaultVideoPoster;
-		private View 		mVideoProgressView;
-    	
-    	@Override
-		public void onShowCustomView(View view, WebChromeClient.CustomViewCallback callback)
-		{
-			//Log.i(LOGTAG, "here in on ShowCustomView");
-	        HTML5WebView.this.setVisibility(View.GONE);
-	        
-	        // if a view already exists then immediately terminate the new one
-	        if (mCustomView != null) {
-	            callback.onCustomViewHidden();
-	            return;
-	        }
-	        
-	        mCustomViewContainer.addView(view);
-	        mCustomView = view;
-	        mCustomViewCallback = callback;
-	        mCustomViewContainer.setVisibility(View.VISIBLE);
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			if ((mCustomView == null) && canGoBack()) {
+				goBack();
+				return true;
+			}
 		}
-		
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private class MyWebChromeClient extends WebChromeClient {
+		private Bitmap mDefaultVideoPoster;
+		private View mVideoProgressView;
+
+		@Override
+		public void onShowCustomView(View view,
+				WebChromeClient.CustomViewCallback callback) {
+			// Log.i(LOGTAG, "here in on ShowCustomView");
+			HTML5WebView.this.setVisibility(View.GONE);
+
+			// if a view already exists then immediately terminate the new one
+			if (mCustomView != null) {
+				callback.onCustomViewHidden();
+				return;
+			}
+
+			mCustomViewContainer.addView(view);
+			mCustomView = view;
+			mCustomViewCallback = callback;
+			mCustomViewContainer.setVisibility(View.VISIBLE);
+		}
+
 		@Override
 		public void onHideCustomView() {
-			
+
 			if (mCustomView == null)
-				return;	       
-			
+				return;
+
 			// Hide the custom view.
 			mCustomView.setVisibility(View.GONE);
-			
+
 			// Remove the custom view from its container.
 			mCustomViewContainer.removeView(mCustomView);
 			mCustomView = null;
 			mCustomViewContainer.setVisibility(View.GONE);
 			mCustomViewCallback.onCustomViewHidden();
-			
+
 			HTML5WebView.this.setVisibility(View.VISIBLE);
-			
-	        //Log.i(LOGTAG, "set it to webVew");
+
+			// Log.i(LOGTAG, "set it to webVew");
 		}
-		
+
 		@Override
 		public Bitmap getDefaultVideoPoster() {
-			//Log.i(LOGTAG, "here in on getDefaultVideoPoster");	
+			// Log.i(LOGTAG, "here in on getDefaultVideoPoster");
 			if (mDefaultVideoPoster == null) {
 				mDefaultVideoPoster = BitmapFactory.decodeResource(
 						getResources(), R.drawable.default_video_poster);
-		    }
+			}
 			return mDefaultVideoPoster;
 		}
-		
+
 		@Override
 		public View getVideoLoadingProgressView() {
-			//Log.i(LOGTAG, "here in on getVideoLoadingPregressView");
-			
-	        if (mVideoProgressView == null) {
-	            LayoutInflater inflater = LayoutInflater.from(mContext);
-	            mVideoProgressView = inflater.inflate(R.layout.video_loading_progress, null);
-	        }
-	        return mVideoProgressView; 
-		}
-    	
-    	 @Override
-         public void onReceivedTitle(WebView view, String title) {
-            ((Activity) mContext).setTitle(title);
-         }
+			// Log.i(LOGTAG, "here in on getVideoLoadingPregressView");
 
-         @Override
-         public void onProgressChanged(WebView view, int newProgress) {
-        	 ((Activity) mContext).getWindow().setFeatureInt(Window.FEATURE_PROGRESS, newProgress*100);
-         }
-         
-         @Override
-         public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
-             callback.invoke(origin, true, false);
-         }
-    }
-	
-	private class MyWebViewClient extends WebViewClient {
-	    @Override
-	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	    	Log.i(LOGTAG, "shouldOverrideUrlLoading: "+url);
-	        view.loadUrl(url);
-	        return true;
-	    }
+			if (mVideoProgressView == null) {
+				LayoutInflater inflater = LayoutInflater.from(mContext);
+				mVideoProgressView = inflater.inflate(
+						R.layout.video_loading_progress, null);
+			}
+			return mVideoProgressView;
+		}
+
+		@Override
+		public void onReceivedTitle(WebView view, String title) {
+			((Activity) mContext).setTitle(title);
+		}
+
+		@Override
+		public void onProgressChanged(WebView view, int newProgress) {
+			((Activity) mContext).getWindow().setFeatureInt(
+					Window.FEATURE_PROGRESS, newProgress * 100);
+		}
+
+		@Override
+		public void onGeolocationPermissionsShowPrompt(String origin,
+				GeolocationPermissions.Callback callback) {
+			callback.invoke(origin, true, false);
+		}
 	}
-	
-	static final FrameLayout.LayoutParams COVER_SCREEN_PARAMS =
-        new FrameLayout.LayoutParams( ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+	private class MyWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			Log.i(LOGTAG, "shouldOverrideUrlLoading: " + url);
+			view.loadUrl(url);
+			return true;
+		}
+
+		@Override
+		public void onPageStarted(WebView view, String url, Bitmap favicon) {
+			super.onPageStarted(view, url, favicon);
+			mProgressDialog.show();
+		}
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			super.onPageFinished(view, url);
+			mProgressDialog.dismiss();
+		}
+	}
+
+	static final FrameLayout.LayoutParams COVER_SCREEN_PARAMS = new FrameLayout.LayoutParams(
+			ViewGroup.LayoutParams.MATCH_PARENT,
+			ViewGroup.LayoutParams.MATCH_PARENT);
 }
